864d39ffaf4c4ebb3f5c68c9b7af3bba2e42cf83
Alberto J. Rubio <yebenes9@gmail.com>
2013-09-15 13:16:34 +0200

    Add MarkI tests.

--- src/test/java/MarkTest.groovy
+++ src/test/java/MarkTest.groovy
@@ -1,5 +1,12 @@
 class MarkTest extends GroovyTestCase {
 
+    void 'test 01: Encode with MarkI letter a with wheel position 5'() {
+        def answer = new MarkI(wheelPosition: 5).encode('a')
+        assert 'f' == answer
+    }
 
-
+    void 'test 02: Encode with MarkI letter c with wheel position 5'() {
+        def answer = new MarkI(wheelPosition: 5).encode('c')
+        assert 'h' == answer
+    }
 }
